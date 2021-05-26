f5a4b0de36f35d4a1a573ec9fedba25fdf7acb13
todddn <84818300+todddn@users.noreply.github.com>
2021-05-25 20:10:03 -0400

    Hello.java
    Lab 01

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Devon Todd");
+		System.out.println("Hello");
+		System.out.println("Lab 01");
+		System.out.println("May 25, 2021");
+		System.out.println("CMSC 255 ");
+		System.out.println();
+		
+	}
+}
